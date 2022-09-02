5003bc9659aac3759b5396175bd7d8acb895a466
jacob whitford-bender <jacob@xmentium.com>
2022-09-02 10:14:26 -0600

    feat: hello world

---
+++ formatAsGitBranch.mjs
@@ -0,0 +1,16 @@
+import {readLines} from "https://deno.land/std/io/bufio.ts"
+import {readerFromStreamReader} from "https://deno.land/std/io/streams.ts"
+
+const username = ''
+const token = ''
+const url = ''
+const headers = new Headers ()
+headers.set ('Authorization', `Basic ${btoa (usernameAndToken)}`)
+const response = await fetch (url, {headers})
+
+for await (const l of readLines (readerFromStreamReader (response.body.getReader()))) {
+	if (!/XM/.test (l)) continue
+	const [ticketName, ticketId] = l.split (',')
+	const branchBody = ticketName.replace (/\s+/g, '-').replace (/:/g, '')
+	console.log (`git checkout -b ${ticketId}-${branchBody}`)
+}
