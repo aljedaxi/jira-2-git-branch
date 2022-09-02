067151698f5d49e372f0679277aaa5d0a5e4865a
jacob whitford-bender <jacob@xmentium.com>
2022-09-02 10:31:39 -0600

    feat: support for tickets with commas in their name, added functions

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 edit the username and [token](https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/), as per your circumstances.
 
 to get the url, go to a jira filter and click "Export" > "Export Excel CSV (all fields)". copy paste that link into the url slot.
+
+you're probably also going to want to edit `isValid`, unless your company's name also starts with `XM`.
--- formatAsGitBranch.mjs
+++ formatAsGitBranch.mjs
@@ -1,16 +1,22 @@
 import {readLines} from "https://deno.land/std/io/bufio.ts"
 import {readerFromStreamReader} from "https://deno.land/std/io/streams.ts"
 
-const username = ''
-const token = ''
-const url = ''
+const username = 'jacob@xmentium.com'
+const token = 'CtzALBTClM4LZUyuHk6vCF48'
+const url = 'https://xmentium.atlassian.net/sr/jira.issueviews:searchrequest-csv-all-fields/10004/SearchRequest-10004.csv?atl_token=9a607669-555b-4671-b5c7-452edf2c56f8_1c921f9068183a5758305ce82125f02c567ad272_lin&tempMax=1000'
 const headers = new Headers ()
 headers.set ('Authorization', `Basic ${btoa (`${username}:${token}`)}`)
 const response = await fetch (url, {headers})
 
+const isValid = s => /XM/.test (s)
+
+const getTicketData = l => l.startsWith('"')
+	? [l.split (',').slice (0, 2).join (',').replace (/"|,/g, ''), l.split(',')[2]]
+	: l.split (',')
+
 for await (const l of readLines (readerFromStreamReader (response.body.getReader()))) {
-	if (!/XM/.test (l)) continue
-	const [ticketName, ticketId] = l.split (',')
+	if (!isValid (l)) continue
+	const [ticketName, ticketId] = getTicketData (l)
 	const branchBody = ticketName.replace (/\s+/g, '-').replace (/:/g, '')
 	console.log (`git checkout -b ${ticketId}-${branchBody}`)
 }
